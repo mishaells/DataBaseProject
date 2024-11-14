38de07719c4ed5ba5c439381b76c8c7e6b7796da
Dhay <159781964+jidhai@users.noreply.github.com>
2024-11-15 00:52:57 +0300

    Update frame3Host.java

--- frame3Host.java
+++ frame3Host.java
@@ -10,6 +10,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class frame3Host extends JFrame {
 
@@ -36,6 +37,7 @@ public class frame3Host extends JFrame {
 	 * Create the frame.
 	 */
 	public frame3Host() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -43,23 +45,26 @@ public class frame3Host extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Select a tabel to Insert,Delete and Update");
+		JLabel lblNewLabel = new JLabel("Select a tabel to Insert,Delete and Update from");
+		lblNewLabel.setFont(new Font("Georgia", Font.PLAIN, 14));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(10, 25, 416, 14);
+		lblNewLabel.setBounds(10, 61, 416, 14);
 		contentPane.add(lblNewLabel);
 		
 		JButton btnNewButton_1 = new JButton("CUSTOMER");
+		btnNewButton_1.setFont(new Font("Georgia", Font.PLAIN, 12));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame4HostCustomer frame = new frame4HostCustomer();
 				frame.setVisible(true);
 			}
 		});
-		btnNewButton_1.setBounds(95, 50, 120, 23);
+		btnNewButton_1.setBounds(97, 86, 120, 23);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton = new JButton("RESERVATION");
-		btnNewButton.setBounds(225, 50, 120, 23);
+		btnNewButton.setFont(new Font("Georgia", Font.PLAIN, 12));
+		btnNewButton.setBounds(225, 86, 120, 23);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame5HostReservation frame = new frame5HostReservation();
@@ -69,13 +74,27 @@ public class frame3Host extends JFrame {
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel_1 = new JLabel("for retrieval operations click here!");
+		lblNewLabel_1.setFont(new Font("Georgia", Font.PLAIN, 14));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setBounds(10, 139, 416, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		JButton btnNewButton_2 = new JButton("here!");
-		btnNewButton_2.setBounds(173, 164, 89, 23);
+		btnNewButton_2.setFont(new Font("Georgia", Font.PLAIN, 14));
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame6HostRetrievals frame = new frame6HostRetrievals();
+				frame.setVisible(true);
+			}
+		});
+		btnNewButton_2.setBounds(97, 164, 248, 36);
 		contentPane.add(btnNewButton_2);
+		
+		JLabel lblNewLabel_2 = new JLabel("Hello host!");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Eras Bold ITC", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(97, 11, 248, 50);
+		contentPane.add(lblNewLabel_2);
 	}
 
 }
